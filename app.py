dae2dc2ad129dd6eb14016eec1cc3056129d0928
Nikhil Malige <nikhilmalige339@gmail.com>
2026-05-26 00:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+    return render_template("search.html", query=query)
+
+@app.route("/profile")
+def profile():
+    user_id = request.args.get("id")
+    return render_template("profile.html", user_id=user_id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
